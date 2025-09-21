6010b77edce9d3d7e8b8b453a4a19fa4722664c1
Marlartate430 <marlartate@gmail.com>
2025-09-21 17:05:51 +0200

    Configurar setup.py para Linux, cambiar requeriments a requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 import os
+def instalar_dependencias_windows(p_path: str, p_requirements: str) -> None:
+    os.system(f'{p_path}\\pip install -r {p_requirements}')
 
-def instalar_dependencias(p_path: str, p_requeriments: str) -> None:
-    os.system(f'{p_path}\\pip install -r {p_requeriments}')
+def instalar_dependencias_posix(p_path: str, p_requiriments: str) -> None:
+    os.system(f'{p_path}/pip install -r {p_requiriments}')
 
 def main() -> None:
-    # Se da por hecho que es Windows
-    ruta_python: str = ".venv\\Scripts"
     ruta_actual: str = f"{os.getcwd()}"
-    mis_requisitos: str = f"{ruta_actual}\\requeriments.txt"
-    instalar_dependencias(ruta_python, mis_requisitos )
+    nombre_archivo_requirements: str = "requirements.txt"
+
+    if os.name == 'nt':
+        ruta_python: str = ".venv\\Scripts"
+        mis_requisitos: str = f"{ruta_actual}\\{nombre_archivo_requirements}"
+        instalar_dependencias_windows(ruta_python, mis_requisitos)
+    elif os.name == 'posix':
+        ruta_python: str = ".venv/Scripts"
+        mis_requisitos: str = f"{ruta_actual}/{nombre_archivo_requirements}"
+        instalar_dependencias_posix(ruta_python, mis_requisitos)
 
 if __name__ == '__main__':
     main()

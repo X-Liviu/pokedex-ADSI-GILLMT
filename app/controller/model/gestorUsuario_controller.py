e14c4028ce80eeb3ddeceb286bf9803d8af6fc3e
marlartet430 <marlartate@gmail.com>
2025-12-23 18:48:19 +0100

    Corregir cambios

--- app/controller/model/gestorUsuario_controller.py
+++ app/controller/model/gestorUsuario_controller.py
@@ -1,5 +1,8 @@
+import sqlite3
+
 class gestorUsuario:
-    def __init__(self, usuario) :
+    def __init__(self, db, usuario) :
+        self.db = db
         self.usuario = usuario
         pass
 
@@ -17,3 +20,26 @@ class gestorUsuario:
 
     def mejorPokemon(self, numEquipo) :
         return self.usuario.mejorPokemon(numEquipo)
+
+    # Marco lo usa en el Ranking, pero pertence a esta clase
+    def aniadirAmigo(self, nombreUsuario: str) -> bool:
+        """
+        pre: Se da por hecho que "nombreUsuario" no esta vacio
+        post: Ainade al usuario que se encuentra como atributo y
+        el que esta como parametro en sus listas de amigos. Si
+        salio bien, devuelve True y en caso contrario False.
+        """
+        resultado: bool = self.usuario.ainadirAmigo(nombreUsuario)
+
+        if resultado:
+            comandosSQL: str = f"""
+             INSERT INTO Amigo_de (NombreUsuario1, NombreUsuario2)
+             VALUES ( {self.usuario.getName()}, {nombreUsuario} );
+            """
+
+            try:
+                self.db.insert(comandosSQL)
+            except sqlite3.Error:
+                resultado = not resultado
+
+        return resultado
